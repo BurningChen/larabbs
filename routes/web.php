e63e9543aaf79bf87a730d56793fe465eb974724
vagrant <vagrant@homestead>
2018-09-27 08:10:27 +0000

    User profile page

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+
+
